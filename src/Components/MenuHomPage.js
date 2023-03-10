5835a032954a9d5afcdacf4806abd72b70c305d8
teo <teo@gmail.com>
2023-03-10 13:21:22 +0700

    Configure React app for deployment to GitHub Pages

--- src/Components/MenuHomPage.js
+++ src/Components/MenuHomPage.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import './../css/MenuHomePage/MenuHomePage.css';
+import logo from '../images/LogoHanwhaLifeWhite.png';
 
 class MenuHomePage extends Component {
     render() {
         return (
             <div className='menu-homepage'>
-                <img src="/images/LogoHanwhaLifeWhite.png" className="logo"></img>
+                {/* <img src={logo} className="logo"></img> */}
+                <div>ABC</div>
                 {this.props.children}
             </div>
         )
